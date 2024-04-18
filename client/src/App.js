d2e1047aca0517b0bff634fb04c788ed5a1db4a7
luka chikashvili <lukachikashvili2020@gmail.com>
2024-04-18 17:05:13 +0400

    3d text animation

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,16 @@
-import { Canvas } from '@react-three/fiber';
+import { Canvas, } from '@react-three/fiber';
+
+import Example from './components/Example';
 
 
 function App() {
 
-  
+   
 
   return (
     <div className="App">
        <Canvas>
-        <mesh position-x = {-2}>
-          <sphereGeometry />
-          <meshBasicMaterial color = "purple" />
-        </mesh>
-
- <mesh rotation-y = {Math.PI * 0.025} position-x = {2} scale = {1.5}>
-  <boxGeometry />
-  <meshBasicMaterial color = "orange" />
- </mesh>
-
-           <mesh position-y = {-1} rotation-x = {-Math.PI * 0.5} scale = {10}>
-             <planeGeometry />
-             <meshBasicMaterial color = "greenYellow" />
-           </mesh>
+          <Example />
        </Canvas>
     </div>
   );
