5f4967c984f4d9e5995596bd31e09a406cb7cd9a
luka chikashvili <lukachikashvili2020@gmail.com>
2024-04-26 18:03:32 +0400

    add camera movements

--- client/src/components/Player.jsx
+++ client/src/components/Player.jsx
@@ -1,14 +1,32 @@
 import { useKeyboardControls } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber';
 import { RigidBody } from '@react-three/rapier'
-import React, { useRef } from 'react'
-
+import React, { useEffect, useRef } from 'react'
+import * as THREE from 'three';
 
 const Player = () => {
     const body = useRef();
     const [subscribeKeys, getKeys] = useKeyboardControls();
 
+
+
+    const jump = () => {
+   
+
+      body.current.applyImpulse({x: 0, y: 0.4, z: 0});
+    }
+
   
+     useEffect(() => {
+        subscribeKeys(
+           (state) => state.jump,
+
+            (value) => {
+              if(value) {jump();}
+            }
+        )
+     }, [])
+
 
     useFrame((state, delta) => {
       const {forward, backward, rightward, leftward} = getKeys();
@@ -42,6 +60,20 @@ const Player = () => {
 
        body.current.applyImpulse(impulse);
       body.current.applyTorqueImpulse(torque);
+
+    const bodyPosition = body.current.translation();
+    const cameraPosition = new THREE.Vector3();
+    cameraPosition.copy(bodyPosition);
+    cameraPosition.z += 2.25;
+    cameraPosition.y += 0.65;
+
+    const cameraTarget = new THREE.Vector3();
+    cameraTarget.copy(bodyPosition);
+    cameraTarget.y += 0.25;
+
+
+    state.camera.position.copy(cameraPosition);
+    state.camera.lookAt(cameraTarget);
 
 
     })
