601852bfeac3f2b8354da5b4758ce9a0d6942f29
luka chikashvili <lukachikashvili2020@gmail.com>
2024-04-26 17:40:06 +0400

    move ball when keyboard button clicked

--- client/src/components/Player.jsx
+++ client/src/components/Player.jsx
@@ -1,20 +1,54 @@
 import { useKeyboardControls } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber';
 import { RigidBody } from '@react-three/rapier'
-import React from 'react'
+import React, { useRef } from 'react'
 
 
 const Player = () => {
-  
+    const body = useRef();
     const [subscribeKeys, getKeys] = useKeyboardControls();
 
-    useFrame(() => {
-      const [forward, backwar, righward, leftward] = getKeys();
+  
+
+    useFrame((state, delta) => {
+      const {forward, backward, rightward, leftward} = getKeys();
+
+      const impulse = { x: 0, y: 0, z: 0};
+      const torque = { x: 0, y: 0, z: 0};
+
+      const impulseStrength = 0.6 * delta;
+      const torqueStrength = 0.2 * delta;
+
+      if(forward) {
+        impulse.z -= impulseStrength;
+        torque.x -= torqueStrength;
+      }
+      
+      if(backward) {
+        impulse.z += impulseStrength;
+        torque.x += torqueStrength;
+      }
+
+      if(leftward) {
+        impulse.x -= impulseStrength;
+        torque.z += torqueStrength;
+      }
+
+      if(rightward) {
+        impulse.x += impulseStrength;
+        torque.z -= torqueStrength;
+      }
+
+
+       body.current.applyImpulse(impulse);
+      body.current.applyTorqueImpulse(torque);
+
+
     })
 
   return (
     <>
-    <RigidBody position={[0, 1, 0]} restitution={0.2} friction={1}>
+    <RigidBody ref={body} position={[0, 1, 0]} canSleep={ false } colliders="ball" restitution={0.2} friction={1}>
       <mesh castShadow>
          <icosahedronGeometry args = { [0.3, 1 ] } />
          <meshStandardMaterial flatShading color = "orange" />
